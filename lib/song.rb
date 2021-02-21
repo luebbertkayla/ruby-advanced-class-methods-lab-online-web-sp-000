ac45072dd288b6b31bce13c745206b22b2379e93
Kayla Luebbert <luebbertkayla@gmail.com>
2021-02-21 23:19:10 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -50,8 +50,8 @@ class Song
     song_array = filename.split(" - ")
     song_array[1] = song_array[1].chomp(".mp3")
     song = self.new 
-    song.artist_name = song[0]
-    song.name = song[1]
+    song.artist_name = song_array[0]
+    song.name = song_array[1]
     song
   end
 end
